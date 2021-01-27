e44f0898e67ee5fb240ab05f71b699539a101ff8
Melchor Dominguez <mdmngz411@gmail.com>
2021-01-27 16:55:14 -0500

    configure eslint

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
