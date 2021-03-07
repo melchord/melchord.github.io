fc57dab7fca68eb33c7b2f5d12f8c9dd7a7286f5
Melchor Dominguez <mdmngz411@gmail.com>
2021-03-07 18:02:35 -0500

    - #8: add github link

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,17 @@ import { getAge } from '../utils';
 
 const Component = () => {
   const age = getAge();
+  const githubLink = <a href='https://github.com/melchord'>Github</a>;
   return (
     <Layout title='Melchor Dominguez - Software Engineer'>
       <div className={styles.title}>Melchor Dominguez</div>
       <h2>I'm a {age} year-old developer</h2>
       <h2>who has used Next.js, React, and others.</h2>
       <br />
-      <h2>Check out my Github.</h2>
+      <h2>Check out my {githubLink}.</h2>
       <br />
-      <div>Currently on the search for my next project.</div>
-      <div>Business inquiry? Email Me!</div>
+      <h2>Currently on the search for my next project.</h2>
+      <h2>Business inquiry? Email Me!</h2>
       <img src='/owl.gif' />
     </Layout>
   );
