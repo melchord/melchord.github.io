3b070a2d7969c505eeac9c0e95dad58190f839d6
melchord <56926559+melchord@users.noreply.github.com>
2021-03-14 03:18:16 +0000

    Deploying to gh-pages from  @ 5310b6f8d2e0325b88c7f7ec5334890aae25adc0 🚀

---
+++ _next/static/HLACLMx97KjXpH9_o3ORy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F62bbf1d28507f65b1fb9.css","static\u002Fchunks\u002Fpages\u002Findex-6fd8b9a4f2cd9b948f1c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
