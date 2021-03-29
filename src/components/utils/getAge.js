adf781eb18d5a36eae0913e94b5e648c282bbce3
Melchor Dominguez <mdmngz411@gmail.com>
2021-03-29 19:52:46 -0400

    - #17: display a more accurate age

--- src/components/utils/getAge.js
+++ src/components/utils/getAge.js
@@ -1,14 +1,6 @@
-const getAge = (currentDate = Date.now(), birthday = 'April 4, 1996') => {
+const getAge = (currentDate = new Date(), birthday = 'April 4, 1996') => {
   const birthDate = new Date(birthday);
-  const ageInMilliseconds = currentDate - birthDate;
-
-  const minutes = 1000 * 60;
-  const hours = minutes * 60;
-  const days = hours * 24;
-  const years = days * 365;
-
-  const age = ageInMilliseconds / years;
-
+  const age = (currentDate - birthDate) / 31557600000;
   return age.toFixed(7);
 };
 
